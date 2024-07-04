20e6b615e1993c80362ebe6b2ccb54b0709301cb
CakuDev <cakudev@gmail.com>
2024-07-04 17:08:40 +0200

    Add group mechanic

--- characters/player/controllable_character/controllable_character.gd
+++ characters/player/controllable_character/controllable_character.gd
@@ -1,7 +1,7 @@
 class_name ControllableCharacter
 extends CharacterBody3D
 
-@export var movement_blocked := false
+@export var can_move := false
 
 
 const SPEED = 5.0
@@ -10,6 +10,7 @@ const LIFT_FORCE = 10.0
 
 
 @onready var model: Model = $"Model"
+@onready var controllable_character_controller: ControllableCharacterController = %ControllableCharacterController
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -20,6 +21,8 @@ var dashed := false
 var dash_multiplier := 2.0
 var current_movement_multiplier := 1.0
 var i = 0
+var group_characters: Array[ControllableCharacter] = []
+var is_grouping := false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -29,46 +32,56 @@ func _physics_process(delta: float) -> void:
 		dashed = false
 		current_movement_multiplier = 1.0
 		
-	if !movement_blocked:
+	if !can_move:
 		idle()
 		move_and_slide()
 		return
 
 	if lift_condition():
 		lift()
+		
+	if group_condition():
+		group()
+	elif ungroup_condition():
+		ungroup()
 
 	if jump_condition():
 		jump(JUMP_VELOCITY)
 
-	var direction := get_input_direction()
-	if direction:
-		move(direction)
-		rotate_model()
-		
-		if not is_on_floor() and Input.is_action_just_pressed("ui_accept"):
-			current_movement_multiplier = dash_multiplier
+	if movement_condition():
+		var direction := get_input_direction()
+		if direction:
 			move(direction)
-			dashed = true
-			i = 0.0
-	else:
-		idle()
-		
-	if not is_on_floor() and dashed:
-		current_movement_multiplier = lerp(2.0, 1.0, i)
-		i += delta
+			rotate_model()
+			
+			if not is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+				current_movement_multiplier = dash_multiplier
+				move(direction)
+				dashed = true
+				i = 0.0
+		else:
+			idle()
+			
+		if not is_on_floor() and dashed:
+			current_movement_multiplier = lerp(2.0, 1.0, i)
+			i += delta
 
 	move_and_slide()
 	
+
+func movement_condition() -> bool:
+	return !is_lifting	
+
 	
 func jump_condition() -> bool:
-	return Input.is_action_just_pressed("ui_accept") and is_on_floor()
+	return Input.is_action_just_pressed("ui_accept") and is_on_floor() and !is_lifting
 	
 	
 func jump(jump_force: float) -> void:
 	velocity.y = jump_force
 	
 	
-func idle():
+func idle() -> void:
 	velocity.x = move_toward(velocity.x, 0, SPEED)
 	velocity.z = move_toward(velocity.z, 0, SPEED)
 	
@@ -91,20 +104,67 @@ func rotate_model() -> void:
 
 
 func lift_condition() -> bool:
-	return is_on_floor() and !is_lifting and Input.is_action_just_pressed("action")
+	return is_on_floor() and !is_lifting and Input.is_action_just_pressed("action") and character_to_lift != null
 
 
-func lift():
+func lift() -> void:
 	character_to_lift.jump(LIFT_FORCE)
+	is_lifting = true
+	await get_tree().create_timer(1.0, true).timeout
+	print("Activating lift")
+	is_lifting = false
+	
+	
+func group_condition():
+	return (
+			controllable_character_controller.characters.size() == group_characters.size() 
+			and can_move 
+			and Input.is_action_just_pressed("action")
+			and !is_grouping
+	)
+	
+	
+func group():
+	for character in controllable_character_controller.characters:
+		character.can_move = true
+	
+	is_grouping = true
+		
+		
+func ungroup_condition():
+	return (
+			Input.is_action_just_pressed("action")
+			and is_grouping
+	)
+		
+
+func ungroup():
+	var characters := controllable_character_controller.characters
+	var active_character := characters[controllable_character_controller.active_character]
+	for character in characters:
+		if character != active_character:
+			character.can_move = false
+	
+	is_grouping = false
 
 
 func _on_action_area_body_entered(body: Node3D) -> void:
 	if body is ControllableCharacter:
-		print("ENTER: " + body.name)
 		character_to_lift = body
 	
 
 func _on_action_area_body_exited(body: Node3D) -> void:
 	if body is ControllableCharacter:
-		print("EXIT: " + body.name)
 		character_to_lift = null
+
+
+func _on_group_area_body_entered(body: Node3D) -> void:
+	if body is ControllableCharacter:
+		group_characters.append(body as ControllableCharacter)
+
+
+func _on_group_area_body_exited(body: Node3D) -> void:
+	if body is ControllableCharacter:
+		group_characters.erase(body as ControllableCharacter)
+		if is_grouping:
+			ungroup()
--- controllers/controllable_character_controller/controllable_character_controller.gd
+++ controllers/controllable_character_controller/controllable_character_controller.gd
@@ -1,30 +1,31 @@
+class_name ControllableCharacterController
 extends Node3D
 
 
 @export var characters: Array[ControllableCharacter]
-var active_character = 0
+var active_character := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	characters[active_character].movement_blocked = true
+	characters[active_character].can_move = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("change_character_left"):
-		characters[active_character].movement_blocked = false
+		characters[active_character].can_move = false
 		
 		if active_character == 0:
 			active_character = characters.size() - 1
 		else:
 			active_character = active_character - 1
 		
-		characters[active_character].movement_blocked = true
+		characters[active_character].can_move = true
 		
 	if Input.is_action_just_pressed("change_character_right"):
-		characters[active_character].movement_blocked = false
+		characters[active_character].can_move = false
 		if active_character == characters.size() - 1:
 			active_character = 0
 		else:
 			active_character = active_character + 1
-		characters[active_character].movement_blocked = true
+		characters[active_character].can_move = true
